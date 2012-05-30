5ab6124903f8a1501600f4523afd92be96f5660c
heewoo <heewoo7139@gmail.com>
2012-05-30 22:52:14 +0900

    화면 연결 v1.0.3

--- PBS/src/com/android/GroupSetting.java
+++ PBS/src/com/android/GroupSetting.java
@@ -1,21 +1,24 @@
 package com.android;
 
+import android.app.AlertDialog;
 import android.app.TabActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.Toast;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
 
 public class GroupSetting extends TabActivity
 {
@@ -25,7 +28,9 @@ public class GroupSetting extends TabActivity
 
 	EditText password;
 	EditText passwordresult;
-	
+
+	EditText mMember;
+
 	Button numberUrl;
 	Button plus;
 	Button plus2;
@@ -35,33 +40,35 @@ public class GroupSetting extends TabActivity
 	CheckBox ch1;
 	CheckBox ch2;
 
+	boolean cnt = false; 
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 
-		
-		
 		TabHost tabHost = getTabHost();
 		LayoutInflater inflater = LayoutInflater.from(this);
 		inflater.inflate(R.layout.groupstting, tabHost.getTabContentView(), true);
 
 		groupname = (EditText) findViewById(R.id.editText7);
 		groupnumber = (EditText) findViewById(R.id.editText8);
-		
-		numberUrl = (Button)findViewById(R.id.button2);
-		
+
+		numberUrl = (Button) findViewById(R.id.button2);
+
 		plus = (Button) findViewById(R.id.button3);
 		plus2 = (Button) findViewById(R.id.button4);
-		
+
 		ch1 = (CheckBox) findViewById(R.id.checkBox1);
 		ch2 = (CheckBox) findViewById(R.id.checkBox2);
 		password = (EditText) findViewById(R.id.editText3);
 		passwordresult = (EditText) findViewById(R.id.editText4);
-		
-		mResultOk = (Button)findViewById(R.id.button5);
-		mCancel = (Button)findViewById(R.id.button7);
-		
+
+		mResultOk = (Button) findViewById(R.id.button5);
+		mCancel = (Button) findViewById(R.id.button7);
+
+		mMember = (EditText) findViewById(R.id.editText5);
+
 		TabSpec tabSpec1 = tabHost.newTabSpec("tab1").setIndicator("내 그룹");
 		tabSpec1.setContent(R.id.tab2);
 		tabHost.addTab(tabSpec1);
@@ -76,7 +83,9 @@ public class GroupSetting extends TabActivity
 		groupnumber.setEnabled(false);
 		plus.setEnabled(false);
 		plus2.setEnabled(false);
-	 
+
+		mMember.setInputType(0);
+
 	}
 
 	@Override
@@ -84,20 +93,18 @@ public class GroupSetting extends TabActivity
 	{
 		super.onResume();
 
-	
-
 		// 비밀번호 체크 박스 눌렸을때 //
 		ch1.setOnCheckedChangeListener(new OnCheckedChangeListener()
 		{
 			public void onCheckedChanged(CompoundButton arg0, boolean arg1)
 			{
-				//체크 됫을때 입력 가능
+				// 체크 됫을때 입력 가능
 				if (ch1.isChecked())
 				{
 					password.setEnabled(true);
 					passwordresult.setEnabled(true);
 				}
-				//체크 안됫을때 입력불가능
+				// 체크 안됫을때 입력불가능
 				else
 				{
 					password.setEnabled(false);
@@ -113,7 +120,7 @@ public class GroupSetting extends TabActivity
 		{
 			public void onCheckedChanged(CompoundButton arg0, boolean arg1)
 			{
-				// 체크 했을때 입력가능 하게 
+				// 체크 했을때 입력가능 하게
 				if (ch2.isChecked())
 				{
 					groupname.setEnabled(true);
@@ -131,41 +138,40 @@ public class GroupSetting extends TabActivity
 				}
 			}
 		});
-		
+
 		// 그룹원 관리 에서 가져오기 버튼을 눌렀을때 전화번호 목록 가져오기//
 		numberUrl.setOnClickListener(new OnClickListener()
-		{			
+		{
 			public void onClick(View arg0)
 			{
-				Intent intent = new Intent(GroupSetting.this,GetPhoneList.class)	;
+				Intent intent = new Intent(GroupSetting.this, GetPhoneList.class);
 				startActivity(intent);
 			}
 		});
-		
+
 		// 관리자 번호 입력 에서 가져오기 버튼을 눌렀을때 전화번호 목록 가져오기//
 		plus2.setOnClickListener(new OnClickListener()
-		{			
+		{
 			public void onClick(View arg0)
-			{			
-				Intent intent1 = new Intent(GroupSetting.this,GetPhoneList.class)	;
+			{
+				Intent intent1 = new Intent(GroupSetting.this, GetPhoneList.class);
 				startActivity(intent1);
 			}
 		});
-		
-		
-		
-		//설정완료 버튼 누를시 설정 완료 하고 내그룹 화면으로 돌아가기
+
+		// 설정완료 버튼 누를시 설정 완료 하고 내그룹 화면으로 돌아가기
 		mResultOk.setOnClickListener(new OnClickListener()
 		{
-			
+
 			public void onClick(View arg0)
 			{
-				 
+
 				Toast.makeText(GroupSetting.this, "설정 완료", Toast.LENGTH_SHORT).show();
 				finish();
 			}
 		});
-		
+
+		// 취소버튼 클릭시
 		mCancel.setOnClickListener(new OnClickListener()
 		{
 			public void onClick(View arg0)
@@ -174,9 +180,29 @@ public class GroupSetting extends TabActivity
 				finish();
 			}
 		});
-		
 
+		// 그룹원 관리에서 그룹원 선택시 (EditText 선택시) 편집할수 있도록 하기
+		mMember.setOnTouchListener(new OnTouchListener()
+		{
+			
+			
+			public boolean onTouch(View arg0, MotionEvent event)
+			{
+				// 그룬원 관리  EditText 를 눌렀을때 발생되는 이벤트 처리 // 이벤트 발생후 수신인 편집 화면으로 이동
+				if(event.getAction() == KeyEvent.ACTION_UP)
+				{
+					 
+					Intent intent = new Intent(GroupSetting.this, PhoneList.class);
+					startActivity(intent);
+					 
+				}
+				 return false;
+			}
+		});
+
+	
+		
+		
 	}
 
- 
 }
